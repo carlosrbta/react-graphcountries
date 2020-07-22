1e5975032d01e1cbd4cc311f83b4ff825fe6d17b
Carlos Roberto Augusto Junior <carlosrbta@gmail.com>
2020-07-22 18:25:59 -0300

    Updates

---
+++ precache-manifest.6b43c9ed7ac664a3b9e0de677a43fa11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383ee6e4f86fa8ab08512be990840f3d",
+    "url": "/react-graphcountries/index.html"
+  },
+  {
+    "revision": "9d3daf90ae5942e73049",
+    "url": "/react-graphcountries/static/css/2.235c98d2.chunk.css"
+  },
+  {
+    "revision": "a3239fda1e7a57b5b8f7",
+    "url": "/react-graphcountries/static/css/main.e56404d2.chunk.css"
+  },
+  {
+    "revision": "9d3daf90ae5942e73049",
+    "url": "/react-graphcountries/static/js/2.74d0e5d8.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/react-graphcountries/static/js/2.74d0e5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3239fda1e7a57b5b8f7",
+    "url": "/react-graphcountries/static/js/main.5b22320b.chunk.js"
+  },
+  {
+    "revision": "76b3740445227b8ec114",
+    "url": "/react-graphcountries/static/js/runtime-main.b7e4149a.js"
+  }
+]);
